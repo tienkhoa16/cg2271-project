fae7376140c3f9b69d8d430072daaa27615c4cd5
Brendan <bwsjsg@gmail.com>
2020-09-18 10:27:50 +0800

    Revert "Motors"
    This reverts commit 9a158eecf7c2964f568d5ee68027cb51d143b395.

--- motors.c
+++ motors.c
@@ -6,44 +6,35 @@ void initMotors(void) {
 	// Enable Clock for PORTD
 	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
 	
-	//  Timer 0 CH0
+	// 
 	PORTD->PCR[RIGHT_MOTOR_FORWARD] &= ~PORT_PCR_MUX_MASK;
 	PORTD->PCR[RIGHT_MOTOR_FORWARD] |= PORT_PCR_MUX(4);
 	
-	// Timer 0 CH3
 	PORTD->PCR[LEFT_MOTOR_FORWARD] &= ~PORT_PCR_MUX_MASK;
 	PORTD->PCR[LEFT_MOTOR_FORWARD] |= PORT_PCR_MUX(4);
-
-	// Timer 0 CH5
+	
 	PORTD->PCR[RIGHT_MOTOR_REVERSE] &= ~PORT_PCR_MUX_MASK;
 	PORTD->PCR[RIGHT_MOTOR_REVERSE] |= PORT_PCR_MUX(4);
-
-	// Timer 0 CH2
+	
 	PORTD->PCR[LEFT_MOTOR_REVERSE] &= ~PORT_PCR_MUX_MASK;
 	PORTD->PCR[LEFT_MOTOR_REVERSE] |= PORT_PCR_MUX(4);
-	
-	// Enable Clock gating for Timer 0
+		
 	SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK;
 	
-	// Select Clock for TPM module
 	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
 	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);
 	
-	// Modulo value 255
 	TPM0->MOD = 0xff;
 	
-	// Enable PWM on TPM1 Channel 0 -> PTB0
 	TPM0->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
 	TPM0->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(1));
 	TPM0->SC &= ~(TPM_SC_CPWMS_MASK);
 	
-	// Enable PWM on TPM channel 0 -> PTD0 and channel 3 -> PTD3
 	TPM0_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
 	TPM0_C3SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
 	TPM0_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
 	TPM0_C3SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
 	
-	// Enable PWM on TPM channel 2 -> PTD2 and channel 5 -> PTD5
 	TPM0_C2SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
 	TPM0_C5SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
 	TPM0_C2SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
@@ -79,7 +70,7 @@ void move(int dir) {
 	} else if (dir == R_RIGHT) {
 		TPM0_C5V = PWM(FAST);				// LEFT REVERSE
 		TPM0_C2V = PWM(SLOW);				// RIGHT REVERSE
-	} else {								// STOP
+	} else {
 		TPM0_C0V = 0;
 		TPM0_C5V = 0;
 		TPM0_C2V = 0;
