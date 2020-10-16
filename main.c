505cc4ac382b23b28f051a03e70fcd9bd1c492a4
tienkhoa16 <nguyentienkhoa22@gmail.com>
2020-10-16 10:03:09 +0800

    Add RTOS for led running

--- main.c
+++ main.c
@@ -1,65 +1,72 @@
-#include "MKL25Z4.h"                    // Device header
+/*----------------------------------------------------------------------------
+ * CMSIS-RTOS 'main' function template
+ *---------------------------------------------------------------------------*/
+ 
+#include "RTE_Components.h"
+#include  CMSIS_device_header
+#include "cmsis_os2.h"
 #include "uart.h"
 #include "ledControl.h"
 
-volatile uint8_t led_count = 0;
+volatile uint32_t rx_data = 0;
+volatile uint32_t led_count = 0;
 uint32_t GREEN_LEDS_STRIP[8] = {GREEN_LED_1, GREEN_LED_2, GREEN_LED_3, GREEN_LED_4, 
         GREEN_LED_5, GREEN_LED_6, GREEN_LED_7, GREEN_LED_8};
-
-/* Delay Function */
-static void delay(volatile uint32_t nof) {
-  while(nof!=0) {
-    __asm("NOP");
-    nof--;
-  }
-}
 
 void UART2_IRQHandler(void) {
     NVIC_ClearPendingIRQ(UART2_IRQn);
     
-    uint8_t rx_data = UART2_D;
-
     if (UART2->S1 & UART_S1_RDRF_MASK) {
+        rx_data = UART2_D;
+    }
+    
+    //Clear INT Flag
+    PORTE->ISFR |= MASK(UART_RX_PORTE23);
+}
+ 
+/*----------------------------------------------------------------------------
+ * Application main thread
+ *---------------------------------------------------------------------------*/
+void tLed(void *argument) {
+    for (;;) {
         if (BLUETOOTH_CONNECTED_MASK(rx_data) == BLUETOOTH_CONNECTED) {
             for (int i = 0; i < 3; i++) {
                 on2GreenLeds();
-                delay(0x120000);
+                osDelay(500);
                 offAllLeds();
-                delay(0x120000);
+                osDelay(500);
             }
+            rx_data = 0;
         } 
 
         if (BUTTON_RELEASED_MASK(rx_data) == BUTTON_RELEASED) {
             onAllGreenLeds();
         }
 
-        while (UP_BUTTON_PRESSED_MASK(rx_data) == UP_BUTTON_PRESSED || 
+        if (UP_BUTTON_PRESSED_MASK(rx_data) == UP_BUTTON_PRESSED || 
                     LEFT_BUTTON_PRESSED_MASK(rx_data) == LEFT_BUTTON_PRESSED || 
                     RIGHT_BUTTON_PRESSED_MASK(rx_data) == RIGHT_BUTTON_PRESSED || 
                     DOWN_BUTTON_PRESSED_MASK(rx_data) == DOWN_BUTTON_PRESSED) {
 
             ledControl(GREEN_LEDS_STRIP[led_count], LED_ON);
-            delay(0x80000);
+            osDelay(50);
             offAllGreenLeds();
 
             led_count = ((led_count == 7) ? 0 : led_count + 1);
-
-            rx_data = UART2_D;
         }
     }
-    
-    //Clear INT Flag
-    PORTE->ISFR |= MASK(UART_RX_PORTE23);
 }
-
-int main(void) {
+ 
+int main (void) {
+ 
+    // System Initialization
     SystemCoreClockUpdate();
     initUART2();
     initLed();
-    
     offAllLeds();
-    while (1) {
-
-    }
-    
+ 
+    osKernelInitialize();                 // Initialize CMSIS-RTOS
+    osThreadNew(tLed, NULL, NULL);    // Create application main thread
+    osKernelStart();                      // Start thread execution
+    for (;;) {}
 }
